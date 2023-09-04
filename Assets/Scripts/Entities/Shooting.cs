e73d139050a9f32649d1d1fb7d8cd062a3f02fd8
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-09-04 17:32:25 +0900

    초기 등록

---
+++ Assets/Scripts/Entities/Shooting.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    private CharacterController _controller;
+    private Animator _anim;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    void Start()
+    {
+        _controller.OnAttackEvent += OnShoot;
+    }
+
+    private void OnShoot()
+    {
+        CreateProjectile();
+    }
+
+    private void CreateProjectile()
+    {
+        _anim.SetTrigger("Attack");
+    }
+}
+
